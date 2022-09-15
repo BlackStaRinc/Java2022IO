9474c8cef34c91e61f120ac0360110d7ddac8555
Hamzz <hamza.aliaskeroglu@gmail.com>
2022-09-15 20:00:51 +0300

    Deneme

---
+++ src/Java2022IO/intro/Main.java
@@ -0,0 +1,43 @@
+package Java2022IO.intro;
+
+
+public class Main {
+    public static void main(String[] args) {
+       
+        //deveCase yazılır
+       String ortaMetin = "İlginizi Çekerim";
+       String altMetin = "Vade Süresi";
+       System.out.println(ortaMetin);
+
+       //integer değişken
+       int vade = 12;
+
+       double dolarDun = 18.20;
+       double dolarBugun = 18.20;
+
+       boolean dolarDustuMu = false;
+
+       String okYonu = "";
+
+       if (dolarBugun < dolarDun) {
+        okYonu = "down.svg";
+        System.out.println(okYonu);
+       } 
+       else if(dolarBugun > dolarDun) {
+        okYonu = "up.svg";
+        System.out.println(okYonu);
+       }
+       else {
+        okYonu = "equal.svg";
+        System.out.println(okYonu);
+       }
+
+       //array
+       String[] krediler = {"Hızlı Kredi","Maaşını Bankadan Alanlar","Mutlu Emekli"};
+     
+       for (int i = 0; i < krediler.length; i++) {
+        System.out.println(krediler[i]);
+       }
+    }
+    
+}
