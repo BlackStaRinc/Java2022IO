e9b8d75c6ad25ed654baf86955575a051790bcb9
Hamzz <hamza.aliaskeroglu@gmail.com>
2022-09-18 19:16:26 +0300

    Fix

--- src/Java2022IO/HW14/Main.java
+++ src/Java2022IO/HW14/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int number = 28;
         int total = 0;
 
-        for (int i = 0; i < number; i++) {
+        for (int i = 1; i < number; i++) {
             if (number % i == 0) {
                 total = total + i;
             }
