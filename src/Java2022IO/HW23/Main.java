4e9326c90176a740b75e53433d7055822eb76bd0
Hamzz <hamza.aliaskeroglu@gmail.com>
2022-10-05 23:29:16 +0300

    NLayerApp

--- src/Java2022IO/HW23/Main.java
+++ src/Java2022IO/HW23/Main.java
@@ -3,7 +3,7 @@ package Java2022IO.HW23;
 public class Main {
     public static void main(String[] args) {
         DortIslem dortIslem = new DortIslem();
-        System.out.println(dortIslem.topla(2, 3));
+        System.out.println(dortIslem.topla(5, 3));
         System.out.println(dortIslem.topla(2, 3, 4));
     }
 }
